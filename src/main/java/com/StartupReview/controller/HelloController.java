530bbe66828fa7e3bf0e6994cd49d97c1183e149
Rajat Kumar <rajat1366@gmail.com>
2021-04-12 15:46:08 +0530

    added network in yml file

--- src/main/java/com/StartupReview/controller/HelloController.java
+++ src/main/java/com/StartupReview/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.StartupReview.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
 @RestController
+@CrossOrigin
 public class HelloController {
     @GetMapping("/api/hello")
     public String hello() {
