18b0007069877b6b4b0c1402de6ded83a41d5154
Rajat Kumar <rajat1366@gmail.com>
2021-04-12 12:30:36 +0530

    First commit

---
+++ src/main/java/com/StartupReview/StartupReviewApplication.java
@@ -0,0 +1,13 @@
+package com.StartupReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartupReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/StartupReview/StartupReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StartupReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
