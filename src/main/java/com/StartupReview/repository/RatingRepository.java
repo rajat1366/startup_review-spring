ed1e6bf4774bde9282c3821deaa2ae810cf8abab
himanshu <himanshusingh6398@gmail.com>
2021-04-25 18:15:12 +0530

    startup crud complete and rating crud started

--- src/main/java/com/StartupReview/repository/RatingRepository.java
+++ src/main/java/com/StartupReview/repository/RatingRepository.java
@@ -1,9 +1,13 @@
 package com.StartupReview.repository;
 
 import com.StartupReview.models.Rating;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RatingRepository extends JpaRepository<Rating,Long> {
+
+    Page<Rating> findAllByOrderByDateTimeDesc(Pageable pageable);
 }
